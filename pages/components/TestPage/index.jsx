296ef25c9cef3c7bd120992c9043298a0843ff46
Yunys26 <aliev-unys@rambler.ru>
2021-03-30 18:43:47 +0300

    init

---
+++ pages/components/TestPage/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function TestPage(props) {
+    return (
+        <div>
+            12312321312
+        </div>
+    )
+}
+
+TestPage.propTypes = {
+
+}
+
+export default TestPage
+
---
+++ pages/rooms.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Rooms(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Rooms.propTypes = {
+
+}
+
+
